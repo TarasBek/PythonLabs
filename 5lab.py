eb1d86866a7f49ceb6bfabed622ccf48749790a4
TarasBek <tarasbek2002@gmail.com>
2021-06-11 06:00:57 +0300

    adding files to  5 lab

---
+++ 5lab.py
@@ -0,0 +1,19 @@
+import re
+
+
+def main():
+    file_with_logs = open("acces_log.txt", "r")
+    count = 0
+    searched_lines = (
+        "\[(0[8-9]|10)/Mar/2004:(0[2-9]|1[0-8]):([1-3][1-9]|40|41)"
+        ':(3[7-9]|4[0-9]|5[0-2]) -0800] ".*" 200'
+    )
+
+    for strings in file_with_logs:
+        for certain_string in re.finditer(searched_lines, strings):
+            count += 1
+    print(count)
+
+
+if __name__ == "__main__":
+    main()
---
+++ README.txt
@@ -0,0 +1,12 @@
+Завдання:
+Порахувати кількість успішних запитів для кожної машини-ініціатора запиту в проміжку 08/Mar/2004:02:11:37 до 10/Mar/2004:18:41:52
+
+Лог файл
+https://drive.google.com/open?id=0B1k21nkk13PYMkxCY09FTVdqcjg
+
+Як запустити програму?
+- введіть "git clone https://github.com/TarasBek/PythonLabs.git"
+- скачайте лог файл
+- в ide в папці lab5 напишіть python 5lab.py
+
+ 
